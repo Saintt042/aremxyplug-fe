a60579214298fb3b67fb82b179d46664f1e95619
Alinwachukwu Michael <saintmichaell42@gmail.com>
2023-04-16 23:00:46 +0100

    added fonts

--- aremxyplug/src/components/Navbar/Navbar.jsx
+++ aremxyplug/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Container } from './NavbarStyle';
+import navLogo from "../../assets/navbarLogo.svg"
 
 
 const Navbar = () => {
   return (
     <Container>
-        <div>Navbar</div>
+        <div>
+          <h1><img src={navLogo} alt="logo" /></h1>
+        </div>
     </Container>
   )
 }
