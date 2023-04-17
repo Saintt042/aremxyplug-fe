6813b91a24454090b3ec4fe8e102b8ed7548cadf
nomeo-310 <onome962@gmail.com>
2023-04-17 21:40:55 +0100

    service page created

---
+++ aremxyplug/src/pages/services/ServiceData.js
@@ -0,0 +1,39 @@
+export const ServiceData = [
+    {
+        id: 1,
+        subheading: 'Telecom',
+        subheadingText: "Looking for a trustworthy, cost-effective Telecommunication service provider? You've found it! Our Telecom services offers fast and secure connectivity for all your telecommunication needs. Whether you need to make calls, send messages, or access the internet, our service has got you covered. We are the telecom company you can trust, with affordable prices and excellent customer support. Sign up today and start enjoying seamless Telecom service!",
+        subheadingLink: '/services/telecom-services'
+    },
+    {
+        id: 2,
+        subheading: 'Payment',
+        subheadingText: "Thanks for visiting our payment services! We offer quick and secure payment processing to make sure your transactions go off without a hitch. Using our user-friendly interface, you can quickly make payments using a number of payment methods, such as local and international bank transfers, ATM card payments, and e-wallet systems. To assist you with any questions you may have regarding payments, our team of experts is committed to offering outstanding service and support. Our team will handle your payments with the utmost security and care.",
+        subheadingLink: '/services/payment-services'
+    },
+    {
+        id: 3,
+        subheading: 'Card Issuing',
+        subheadingText: "We provide customers with a convenient and secure way to make purchases both online and in-store with our built-in security chip and PIN technology to prevent fraud and protect your sensitive information with AremxyPlug’s card issuing service. Our card issuing service can be tailored to your specific needs as an Individual or business, we offer 3D secured virtual and physical debit USD and NGN cards to enable individuals and businesses to spend their cards anywhere Verve, Visa, and Mastercard are accepted, both in-store and online, anytime, anywhere.",
+        subheadingLink: '/services/card-issuing'
+    },
+    {
+        id: 4,
+        subheading: 'VTU Development',
+        subheadingText: "AremxyPlug is a company that has a team of experts that can easily and quickly design and develop a VTU platform to start your telecom business with just a few steps and line of codes. You can set up a VTU website to run your telecom business even as a business owner, student, or an individual with services like mobile airtime top-up, data bundles, education pins, TV subscription, and bills payment platform.",
+        subheadingLink: '/services/vtu-development'
+    },
+    {
+        id: 5,
+        subheading: 'Digital Services',
+        subheadingText: "The stress of sourcing multiple digital service providers is a thing of the past. Why? Because with AremxyPlug, you can source for professionals from every field devoted to developing your business and catering to your digital needs. We offer a variety of digital services that can be customized to fit your company's needs. Our services are affordable and designed to deliver outstanding results for your company.",
+        subheadingLink: '/services/digital-services'
+    },
+    {
+        id: 6,
+        subheading: 'Business Development',
+        subheadingText: "With the help of our highly regarded Digital Strategy, Our team of experts will develop the best strategies to assist you in developing, implementing, and enhancing digital business capabilities within your company. When it comes to business development, trust us to save you time and stress.",
+        subheadingLink: '/services/business-development'
+    },
+
+]
--- aremxyplug/src/routers/Routers.js
+++ aremxyplug/src/routers/Routers.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import Signup from '../pages/Signup/Signup';
+import Services from '../pages/services/Services';
+import SingleServices from '../pages/services/SingleServices';
 
 
 
@@ -11,6 +13,10 @@ const Routers = () => {
         <Route path='/' element={<Navigate to="home" />} /> 
         <Route path='home' element={<Home />} />
         <Route path='signup' element={<Signup />} />
+        <Route path='/services'>
+          <Route index element={<Services/>}/>
+          <Route path=':id' element={<SingleServices/>}/>
+        </Route>
     </Routes>
   )
 }
